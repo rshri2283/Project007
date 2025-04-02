545b737afc9a3610511e5f3724eb933803aeefae
rohan <root@manager.lab.example.com>
2025-04-02 12:43:49 +0530

    Condition example for day shopping

---
+++ conditions_example1.py
@@ -0,0 +1,10 @@
+day=input("Enter the day:")
+amount=int(input("Enter the amount you shopped for:"))
+
+if day=="sunday" and amount>=5000:
+  discount=(amount*10)/100
+  amount=amount-discount
+  print("After discount the amount will be:", amount)
+else:
+  print("Without discount the price will be:", amount)
+
