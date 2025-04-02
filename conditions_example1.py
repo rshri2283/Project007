dca6636bc67aaa2e05674ee1e8d78386d61e03df
rohan <root@manager.lab.example.com>
2025-04-02 12:50:13 +0530

    Added conversion of string into lowercase

--- conditions_example1.py
+++ conditions_example1.py
@@ -1,7 +1,7 @@
 day=input("Enter the day:")
 amount=int(input("Enter the amount you shopped for:"))
 
-if day=="sunday" and amount>=5000:
+if day.lower()=="sunday" and amount>=5000:
   discount=(amount*10)/100
   amount=amount-discount
   print("After discount the amount will be:", amount)
